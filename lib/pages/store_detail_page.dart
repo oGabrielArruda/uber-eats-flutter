0d228323a16ab9ed13b3d0544f8b1cc65de8ecbe
oGabrielArruda <47468017+oGabrielArruda@users.noreply.github.com>
2021-03-08 08:30:05 -0300

    footer store detail

--- lib/pages/store_detail_page.dart
+++ lib/pages/store_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uber_eats_flutter/theme/colors.dart';
 
 class StoreDetailPage extends StatefulWidget {
   final String img;
@@ -12,6 +13,29 @@ class StoreDetailPage extends StatefulWidget {
 class _StoreDetailPageState extends State<StoreDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomSheet: getFooter(),
+    );
+  }
+
+  Widget getFooter() {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      height: 80,
+      width: size.width,
+      decoration: BoxDecoration(
+          color: white,
+          border: Border(top: BorderSide(color: black.withOpacity(0.1)))),
+      child: Padding(
+        padding: EdgeInsets.only(top: 15),
+        child: Column(
+          children: [
+            Text("\$15.00 away from a \$0.00 delivery fee.",
+                style: TextStyle(
+                    fontSize: 14, fontWeight: FontWeight.w500, color: primary)),
+          ],
+        ),
+      ),
+    );
   }
 }
