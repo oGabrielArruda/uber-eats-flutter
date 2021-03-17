17db31f852f4bebcda5054f35600c0bdf2cf58fe
oGabrielArruda <47468017+oGabrielArruda@users.noreply.github.com>
2021-03-17 08:18:01 -0300

    info about delievery fee

--- lib/pages/store_detail_page.dart
+++ lib/pages/store_detail_page.dart
@@ -254,7 +254,60 @@ class _StoreDetailPageState extends State<StoreDetailPage> {
                         );
                       },
                     )),
-                  )
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    width: size.width - 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        color: black.withOpacity(0.1),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Delievery Fee",
+                              style: TextStyle(
+                                color: black.withOpacity(0.5),
+                              )),
+                          SizedBox(height: 15.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: (size.width - 100) * 0.8,
+                                child: Text(
+                                  "There aren't enough couriers nearby, so the delievery fee is higher right now",
+                                  style: TextStyle(fontSize: 14, height: 1.5),
+                                ),
+                              ),
+                              Container(
+                                width: (size.width - 70) * 0.2,
+                                child: Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: black.withOpacity(0.15),
+                                  ),
+                                  child: Center(
+                                    child: Icon(Icons.arrow_circle_up_outlined,
+                                        color: black.withOpacity(0.4),
+                                        size: 14),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Divider(color: black.withOpacity(0.3)),
                 ],
               ),
             ),
